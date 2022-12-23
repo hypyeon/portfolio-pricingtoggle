d80adb03a04d69660f87bfd071bd6520a821e24f
hypyeon <hayeong.pyeon@gmail.com>
2022-12-22 19:39:32 -0800

    mobile responsive

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
-let annualPrice = document.querySelectorAll('.annual');
-let monthlyPrice = document.querySelectorAll('.monthly');
-let checkBox = document.querySelector('#input'); 
+let annualPrice = document.getElementsByClassName('annual');
+let monthlyPrice = document.getElementsByClassName('monthly');
+let checkBox = document.getElementById('input'); 
 
-function showPrices() {
-    if (checkBox.checked) {
-        annualPrice.toggle('active');
-        monthlyPrice.toggle('remove');
-    } else {
-        monthlyPrice.toggle('active');
-        annualPrice.toggle('remove');
-    };
-};
